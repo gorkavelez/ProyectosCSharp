f19fc0df65a18213da65bad210e4887914a78493
gorkavelez@yahoo.es <Github24011944>
2018-03-06 17:44:23 +0100

    Cola proyectos

--- SaltoConsoleAPPTest/SaltoConsoleAPPTest/Program.cs
+++ SaltoConsoleAPPTest/SaltoConsoleAPPTest/Program.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using SaltoConsoleAPPTest.PageItem;
+using System.Net;
+using System.Web.Services;
+using System.Web;
+
 
 namespace SaltoConsoleAPPTest
 {
@@ -10,7 +15,31 @@ namespace SaltoConsoleAPPTest
     {
         static void Main(string[] args)
         {
-            int i = 12;            
+            Program ClasPrograma = new Program();
+            ClasPrograma.leerProductos();
+        }
+
+        public void leerProductos()
+        {
+            Item_Service ServItem = new Item_Service();
+            
+            ServItem.UseDefaultCredentials=true;
+            Item_Filter vfiltroNo = new Item_Filter();
+            vfiltroNo.Field = Item_Fields.No;
+            vfiltroNo.Criteria = " ";           
+
+            Item_Filter[] vfiltros = new Item_Filter[] { vfiltroNo };//,vfiltroFecha};                       
+            
+            PageItem.Item[] vItem = ServItem.ReadMultiple(vfiltros, "", 0);
+
+            foreach(Item producto in vItem)
+            {
+                if (producto.Description != null)
+                    Console.WriteLine(producto.No + " - " + producto.Description.ToString());
+            }
+            Console.Write("\nPress any key to continue... ");
+            Console.ReadLine();           
+            
         }
     }
 }
