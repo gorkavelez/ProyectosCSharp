187e78da3f744a00dc0d795ed0d072ab9a85f82e
gorkavelez <gorkavelez@yahoo.es>
2018-03-07 11:47:51 +0100

    subida cola proyectos

---
+++ ColaProyectos/ColaProyectos/ColaProyectos/Entidad.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ColaProyectos.WSLanzarCola;
+
+namespace ColaProyectos
+{
+    public class Entidad
+    {
+        public void escribirLog(string errMsg, string path, bool fechaHora = false)
+        {
+            try
+            {
+                if (fechaHora)
+                {
+                    //System.Globalization.CultureInfo enUS = new CultureInfo("en-US");
+                    if (System.Globalization.CultureInfo.CurrentCulture.ToString() == "en-US")
+                    {
+                        errMsg = System.DateTime.Now.AddHours(6).ToString("dd/MM/yyyy HH:mm:ss", System.Globalization.CultureInfo.CurrentCulture) + " " + errMsg;
+                    }
+                    else
+                    {
+                        errMsg = System.DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss", System.Globalization.CultureInfo.CurrentCulture) + " " + errMsg;
+                    }
+                }
+
+                if (!System.IO.File.Exists(path))
+                {
+                    System.IO.File.Create(path).Dispose();
+                    System.IO.TextWriter tw = new System.IO.StreamWriter(path);
+                    tw.WriteLine(errMsg);
+                    //tw.Close();
+                    tw.Flush();
+                    tw.Close();
+                    tw.Dispose();
+                }
+                else
+                {
+                    System.IO.TextWriter tw = new System.IO.StreamWriter(path, true);
+                    tw.WriteLine(errMsg);
+                    //tw.Close();
+                    tw.Flush();
+                    tw.Close();
+                    tw.Dispose();
+                }
+            }
+            catch (System.Exception ex)
+            {
+                               
+
+            }            
+
+        }
+    }
+}
--- ColaProyectos/ColaProyectos/ColaProyectos/Program.cs
+++ ColaProyectos/ColaProyectos/ColaProyectos/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ColaProyectos.WSLanzarCola;
 
 namespace ColaProyectos
 {
@@ -10,7 +11,26 @@ namespace ColaProyectos
     {
         static void Main(string[] args)
         {
-                       
+
+            string Path =@"C:\PDF\Log.txt";
+            Entidad cssEntidad = new Entidad();
+            WSLanzarCola.LanzarColaProyecto SrvLanzar = new LanzarColaProyecto();            
+            SrvLanzar.UseDefaultCredentials = true;
+            string msgError = string.Empty;
+            try
+            {
+                if (SrvLanzar.LanzarColaTrabajo(ref msgError))
+                {                    
+                    cssEntidad.escribirLog(msgError, Path, true);
+                }
+                else
+                    cssEntidad.escribirLog(msgError, Path, true);
+            }
+            catch(Exception ex)
+            {
+                cssEntidad.escribirLog(ex.Message, Path, true);
+            }
         }
+
     }
 }
